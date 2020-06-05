76c377d0341168f6b4933055708d4b99f32a096b
Ignacio Garcia <nachog.2000@hotmail.com>
2020-06-05 14:21:40 -0300

    Punto 1

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,3 +2,33 @@ module Lib where
 import Text.Show.Functions
 
 laVerdad = True
+
+-- 1)
+type Gema = Personaje -> Personaje
+
+data Guantelete = UnGuantelete {
+    gemas :: [Gema],
+    material :: String
+}deriving (Show)
+
+data Personaje = UnPersonaje {
+    edad :: Int,
+    energia :: Int,
+    habilidades :: [String],
+    nombre :: Int,
+    planeta :: Int
+}
+
+type Universo = [Personaje]
+
+
+chasquear :: Guantelete -> Universo -> Universo
+chasquear guantelete universo | guanteleteCompleto guantelete = reducirMitad universo
+                              | otherwise = universo
+
+guanteleteCompleto :: Guantelete -> Bool
+guanteleteCompleto guantelete = ((==6).length.gemas) guantelete && ((=="uru").material) guantelete
+
+reducirMitad :: Universo -> Universo
+reducirMitad universo = take (length universo `div` 2) universo
+                             
